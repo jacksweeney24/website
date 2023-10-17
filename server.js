da91eeb39ce9cd69e1dbcc76828bb954015750d5
jacksweeney24 <jacksweeney24@gmail.com>
2023-10-17 16:43:52 -0500

    yugh

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const mainDomainStatic = express.static('main-domain');
+const subDomainStatic = express.static('sub-domain');
 
-// Middleware to handle requests for the main domain
 app.use((req, res, next) => {
   if (req.hostname === 'jacksweeney.xyz') {
-    express.static('main-domain')(req, res, next);
+    mainDomainStatic(req, res, next);
   } else {
     next();
   }
 });
 
-// Middleware to handle requests for the subdomain
 app.use((req, res, next) => {
   if (req.hostname === 'services.jacksweeney.xyz') {
-    express.static('sub-domain')(req, res, next);
+    subDomainStatic(req, res, next);
   } else {
     next();
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
