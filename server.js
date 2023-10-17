050c33842cbd7e5ca6d8a922eb90c58d97cb0e88
jacksweeney24 <jacksweeney24@gmail.com>
2023-10-17 17:57:06 -0500

    YES

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const mainDomainStatic = express.static('main-domain');
-const subDomainStatic = express.static('sub-domain');
 
+const PORT = process.env.PORT || 3000;
+
+// Middleware for main domain
 app.use((req, res, next) => {
-  if (req.hostname === 'jacksweeney.xyz') {
-    mainDomainStatic(req, res, next);
+  if (req.hostname === 'jacksweeney.xyz' || req.hostname === 'localhost') {
+    express.static(path.join(__dirname, 'main-domain'))(req, res, next);
   } else {
     next();
   }
 });
 
+// Middleware for subdomain
 app.use((req, res, next) => {
   if (req.hostname === 'services.jacksweeney.xyz') {
-    subDomainStatic(req, res, next);
+    express.static(path.join(__dirname, 'sub-domain'))(req, res, next);
   } else {
     next();
   }
 });
 
+// Route handler for main domain
+app.get('/', (req, res, next) => {
+  if (req.hostname === 'jacksweeney.xyz' || req.hostname === 'localhost') {
+    res.sendFile(path.join(__dirname, 'main-domain', 'index.html'));
+  } else {
+    next();
+  }
+});
+
+// Route handler for subdomain
 app.get('/', (req, res) => {
-  if(req.hostname === 'jacksweeney.xyz' || req.hostname === 'localhost') {
-    res.sendFile(__dirname + '/main-domain/index.html');
-  } else if(req.hostname === 'services.jacksweeney.xyz') {
-    res.sendFile(__dirname + '/sub-domain/index.html');
+  if (req.hostname === 'services.jacksweeney.xyz') {
+    res.sendFile(path.join(__dirname, 'sub-domain', 'index.html'));
   } else {
     res.status(404).send('Not Found');
   }
 });
 
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
