16d0bfa1d940a3def2f6f079bac03120a5650d31
jacksweeney24 <jacksweeney24@gmail.com>
2023-10-17 17:02:39 -0500

    teeesty

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 const mainDomainStatic = express.static('main-domain');
 const subDomainStatic = express.static('sub-domain');
 
@@ -15,4 +17,19 @@ app.use((req, res, next) => {
   } else {
     next();
   }
+});
+
+app.get('/', (req, res) => {
+  if(req.hostname === 'jacksweeney.xyz') {
+    res.sendFile(__dirname + '/main-domain/index.html');
+  } else if(req.hostname === 'services.jacksweeney.xyz') {
+    res.sendFile(__dirname + '/sub-domain/index.html');
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
